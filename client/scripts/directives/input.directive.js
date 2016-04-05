153e31591b4982e435eac6e1820f92e54cbf7757
Cody <meanderingmonarch@gmail.com>
2016-04-05 09:51:32 -0500

    Wired up send message and keyboard commands

---
+++ client/scripts/directives/input.directive.js
@@ -0,0 +1,54 @@
+angular
+	.module('Whatsapp')
+	.directive('input', input);
+
+	//Directive enables sneding message when tapping return
+	// and expose the focus and blur events to adjust the view when the keyboard opens and closes
+
+	function input ($timeout){ 
+		return {
+			restrict: "E",
+			scope: {
+				"returnClose": '=',
+				'onReturn': '&',
+				'onFocus': '&',
+				'onBlue': '&'
+			},
+			linke: link
+		};
+
+
+///
+///
+		function link (scope, element, attrs){
+			element.bind('focus', function(e){
+				if (!scope.onFocus) return;
+
+				$timeout(function () {
+					scope.onFocus();
+				});
+			});
+
+			element.bind('blur', function(e){
+				if (!scope.onBlur) return;
+
+				$timeout(function () {
+					scope.onBlur();
+				});
+			});
+
+			element.bind('keydown', function(e){
+				if (e.which != 13) return;
+
+				if (scope.returnClose){
+					element[0].blur();
+				}
+
+				if (scope.onReturn){
+					$timeout(function (){
+						scope.onReturn();
+				});
+				}
+			});
+		}
+	}

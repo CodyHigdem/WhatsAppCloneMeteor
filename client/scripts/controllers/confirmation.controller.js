595bd8c40ed625b47f0a4cc5d7c1c41685ad616c
Cody <meanderingmonarch@gmail.com>
2016-04-05 14:34:37 -0500

    Created the controller for confirmation

---
+++ client/scripts/controllers/confirmation.controller.js
@@ -0,0 +1,25 @@
+angular
+	.module('Whatsapp')
+	.controller('ConfirmationCtrl', ConfirmationCtrl);
+
+	//
+
+	function confirm() {
+		if (_.isEmpty(this.code)) return;
+
+		Accounts.verifyPhone(this.phone, this.code, function(err) {
+			if (err) return handleError(err);
+			$state.go('profile');
+		});
+	}
+
+	function handleError(err){
+		$log.error('Verification error ', err);
+
+		$ionicPopup.alert({
+			title: err.reason || 'Verification failed', 
+			template: 'Please try again',
+			okType: 'button-positive button-clear'
+		});
+	}
+}
